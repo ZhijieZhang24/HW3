8a832c7f63e36e82c6775fc154dcf65ddfac224f
ZhijieZhang24 <54722530+ZhijieZhang24@users.noreply.github.com>
2019-11-01 11:52:04 -0500

    remove original calculator

--- app.py
+++ app.py
@@ -5,19 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle='My Calculator')
-    return render_template('index.html', display="Your Discount Factor: ", display2="Your loan payment: ", pageTitle= 'Loan Calculator')
+        return render_template('index.html', display="Your Discount Factor: ", display2="Your loan payment: ", pageTitle= 'Loan Calculator')
 
-@app.route('/add', methods=['GET', 'POST'])
+
 @app.route('/divide', methods=['GET', 'POST'])
 def addition():
     if request.method == 'POST':
       form = request.form
       try:
-          numberOne = int(form['numOne'])
-          numberTwo= int(form['numTwo'])
-          calc = numberOne + numberTwo
-          return render_template('index.html', display=calc, pageTitle='My Calculator')
           A = float(form['A'])
           n = int(form['n'])
           i = float(form['i'])
@@ -27,8 +22,6 @@ def addition():
           return render_template('index.html', display=D,display2=P,pageTitle='Loan Calculator')
 
       except ValueError:
-          return render_template('index.html', display="Please enter two integers.", pageTitle='My Calculator')
-
           return render_template('index.html', display="Please enter right number.", display2="Error",pageTitle='Loan Calculator')
 
     return redirect("/")
