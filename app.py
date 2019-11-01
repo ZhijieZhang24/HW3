d0ed13c5f76b8b4c273be7105147040224f016a7
ZhijieZhang24 <54722530+ZhijieZhang24@users.noreply.github.com>
2019-11-01 11:23:39 -0500

    using code to get right calculations

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', display="", pageTitle='My Calculator')
-
+    return render_template('index.html', display="Your Discount Factor: ", display2="Your loan payment: ", pageTitle= 'Loan Calculator')
 
 @app.route('/add', methods=['GET', 'POST'])
+@app.route('/divide', methods=['GET', 'POST'])
 def addition():
     if request.method == 'POST':
       form = request.form
@@ -17,11 +18,36 @@ def addition():
           numberTwo= int(form['numTwo'])
           calc = numberOne + numberTwo
           return render_template('index.html', display=calc, pageTitle='My Calculator')
+          A = float(form['A'])
+          n = int(form['n'])
+          i = float(form['i'])
+          D = (((1 + i)**n)-1)/ (i*( 1 + i)**n)
+          P = A/D
+
+          return render_template('index.html', display=D,display2=P,pageTitle='Loan Calculator')
 
       except ValueError:
           return render_template('index.html', display="Please enter two integers.", pageTitle='My Calculator')
 
+          return render_template('index.html', display="Please enter right number.", display2="Error",pageTitle='Loan Calculator')
+
     return redirect("/")
+
+#def divide():
+    #if request.method == 'POST':
+        #form = request.form
+        #try:
+            #A = float(form['A'])
+            #n = int(form['n'])
+            #i = float(form['i'])
+            #D = (((1 + i)**n)-1)/ ( i * ( 1+ i)**n)
+            #P = A/D
+            #return render_template('index.html', display2=P, pageTitle='Loan Calculator')
+
+        #except ValueError:
+            #return render_template('index.html', display2="Error", pageTitle='Loan Calculator')
+
+        #return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
